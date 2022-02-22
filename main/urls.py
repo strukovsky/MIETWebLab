2b4d190c17e9f48963669e4233fc5a5d78481897
Dmitry Strukovsky <strukovsky1@gmail.com>
2022-02-22 09:51:05 +0300

    feat: add templates

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index, grammar, vocabulary, authenticate
 
 urlpatterns = [
     path('', index, name="index"),
+    path('grammar', grammar, name="grammar"),
+    path('vocabulary', vocabulary, name="vocabulary"),
+    path('authenticate', authenticate, name="authenticate"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,18 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+
 def index(request: HttpRequest) -> HttpResponse:
     return render(request, 'Index.html')
+
+
+def vocabulary(request: HttpRequest) -> HttpResponse:
+    return render(request, 'Vocabulary.html')
+
+
+def grammar(request: HttpRequest) -> HttpResponse:
+    return render(request, 'Grammar.html')
+
+
+def authenticate(request: HttpRequest) -> HttpResponse:
+    return render(request, 'Authenticate.html')
